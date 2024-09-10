a67d1ddc2e63258122dba4aaab7abb0dca6866a2
Janne <janne.viljanen@hotmail.com>
2024-09-10 11:54:58 +0300

    Created a flow chart and added variables.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        Random random = new Random ();
+        int randomNumber = random.nextInt(11);
+        String answer = "";
+        
+        System.out.println("Shall we play a game of slots?");
+        System.out.println("Yes/No");
+        if (answer.equalsIgnoreCase("Yes"))
+        {
+            System.out.println("Ok, let's play.");
+        }
     }
 }
